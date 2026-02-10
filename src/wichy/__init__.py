f5f226e2791967ee3826ecd551e3b1ecb78bd9d0
Wilhelm Hansson <wilhelm.hansson@pm.me>
2026-02-11 00:28:57 +0100

    ADD

---
+++ src/wichy/__init__.py
@@ -0,0 +1,10 @@
+"""Agentic LLM for coding."""
+
+__version__ = "0.1.0"
+__author__ = "Wilhelm Hansson"
+__email__ = "jolt-tannery-spool@duck.com"
+__description__ = "Agentic LLM for coding."
+
+from .__main__ import main
+
+__all__ = ["main", "__version__"]
