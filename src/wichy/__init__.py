7345167b4aa86e52a30ea353a4307cac385d3fbd
Wilhelm Hansson <wilhelm.hansson@pm.me>
2026-02-11 16:39:48 +0100

    UPDATE

--- src/wichy/__init__.py
+++ src/wichy/__init__.py
@@ -2,7 +2,7 @@
 
 __version__ = "0.1.0"
 __author__ = "Wilhelm Hansson"
-__email__ = "jolt-tannery-spool@duck.com"
+__email__ = "wilhelm.hansson@pm.me"
 __description__ = "Agentic LLM for coding."
 
 from .__main__ import main
